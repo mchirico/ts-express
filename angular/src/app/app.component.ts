5589d1fed6442bbb77e60b0ffd141337e65abe89
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-06-26 08:11:10 -0400

    Angular Toolbar + Test Actions

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import {Label} from './label'
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,21 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'angular';
   items: Observable<any[]>;
+  label: Label = {
+    id: 1,
+    name: 'Items'
+  };
+  result: Label = {
+    id: 1,
+    name: ''
+  };
   constructor(firestore: AngularFirestore) {
     this.items = firestore.collection('items').valueChanges();
   }
-  
+
+  onClickMe(){
+    this.result.name = this.label.name;
+  }
+  ngOnInit() {
+  }
 }
