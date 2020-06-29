3cb7806073eb7bd9421731c68315241b12391e0f
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-06-29 10:20:40 -0400

    Refactor tests

--- test/firebase.test.ts
+++ test/firebase.test.ts
@@ -4,9 +4,11 @@ import { App } from "../src/firebase";
 
 describe("Firebase ...", function () {
   it("Test write to firebase", async function () {
+    let status = false;
     const app = App();
     const db = app.firestore();
-    db.collection("tsExample")
+    await db
+      .collection("tsExample")
       .doc("y")
       .set({
         first: "What now okay done...",
@@ -16,11 +18,36 @@ describe("Firebase ...", function () {
       })
       .then((r) => {
         console.log("Document written with ID: ", r);
+        expect(r).to.exist;
+        status = true;
       })
       .catch(function (error: Error) {
         console.error("Error adding document: ", error);
       });
 
-    expect("test").to.contain("test");
+    expect(status).to.be.true;
+  });
+
+  it("Test called twice", async function () {
+    const app = App();
+    const db1 = app.firestore();
+    const db = app.firestore();
+    expect(db1).to.equal(db);
+    await db
+      .collection("tsExample")
+      .doc("y")
+      .set({
+        first: "What now okay done...",
+        last: "Lovelace",
+        born: 1815,
+        msg: "Hello world",
+      })
+      .then((r) => {
+        console.log("Document written with ID: ", r);
+        expect(r).to.exist;
+      })
+      .catch(function (error: Error) {
+        console.error("Error adding document: ", error);
+      });
   });
 });
