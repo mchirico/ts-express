70c5e6ffe12f3eefddb8d8ff78e86ed28379b72c
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-06-29 09:10:52 -0400

    More tests

--- angular/src/app/service/increment-decrement.service.spec.ts
+++ angular/src/app/service/increment-decrement.service.spec.ts
@@ -13,4 +13,29 @@ describe('IncrementDecrementService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('should be minimum', () => {
+    service.value = service.min - 1;
+    service.decrement();
+    expect(service.message).toEqual('Minimum reached!');
+    expect(service.value).toEqual(service.min - 1);
+    service.increment();
+    expect(service.value).toEqual(service.min );
+    expect(service.message).toEqual(
+      `You have ${service.max - service.value} before max`
+    );
+
+
+  });
+  it('should be maximum', () => {
+    service.value = service.max + 1;
+    service.increment();
+    expect(service.message).toEqual('Maximum reached!');
+    expect(service.value).toEqual(service.max + 1);
+    service.decrement();
+    expect(service.value).toEqual(service.max );
+    expect(service.message).toEqual('');
+
+  });
 });
