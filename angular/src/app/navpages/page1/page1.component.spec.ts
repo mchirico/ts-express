887d2f22de6ad71dd07fff3b2932abeedfb5d3cc
Mike Chirico <mchirico@gmail.com>
2020-06-29 09:32:58 -0400

    More tests (#25)

--- angular/src/app/navpages/page1/page1.component.spec.ts
+++ angular/src/app/navpages/page1/page1.component.spec.ts
@@ -3,19 +3,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Page1Component } from './page1.component';
 import {IncrementDecrementService} from '../../service/increment-decrement.service';
 
+class TestIncrementDecrementService{
+  called: string[] = [];
+  increment(): void {
+    this.called.push('increment');
+  }
+  decrement(): void{
+    this.called.push('decrement');
+  }
+  get status(): string[] {
+    return this.called;
+  }
+}
 
 describe('Page1Component', () => {
   let component: Page1Component;
   let fixture: ComponentFixture<Page1Component>;
-
-
-
-
+  const testIncrementDecrementService = new TestIncrementDecrementService();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ Page1Component ],
-      providers: [ IncrementDecrementService ]
+      providers: [
+        {provide: IncrementDecrementService, useValue: testIncrementDecrementService}  ]
     })
     .compileComponents();
   }));
@@ -26,9 +36,17 @@ describe('Page1Component', () => {
     fixture.detectChanges();
   });
 
-
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should call increment and decrement', () => {
+    component.increment();
+    component.decrement();
+    expect(testIncrementDecrementService.status).toEqual(['increment',
+    'decrement']);
+
+  });
+
 });
